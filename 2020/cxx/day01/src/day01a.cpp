4f81c3c4cd9059c03fc3af2c72907a714d04ec00
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2020-12-10 17:41:08 +0000

    Add solution

--- 2020/cxx/day01/src/day01a.cpp
+++ 2020/cxx/day01/src/day01a.cpp
@@ -4,6 +4,7 @@
 
 #include <cstdlib>
 // using std::exit
+// using std::atoi
 
 #include <iostream>
 // using std::cout
@@ -12,13 +13,42 @@
 #include <fstream>
 // using std::ifstream
 
-#include <vector>
-// using std::vector
+#include <set>
+// using std::set
+
+#include <optional>
+// using std::optional
+
+auto find_desired_sum_parts(const std::set<int> &input, int desired_sum) {
+  bool found = false;
+  int complement = 0;
+
+  for (auto &e : input) {
+    complement = desired_sum - e;
+
+    if (input.count(complement)) {
+      found = true;
+      break;
+    }
+  }
+
+  return found ? std::make_optional(
+                     std::make_pair(desired_sum - complement, complement))
+               : std::nullopt;
+}
+
+int part1(const std::set<int> &input) {
+  if (auto p = find_desired_sum_parts(input, 2020)) {
+    return p->first * p->second;
+  }
+
+  return 0;
+}
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     std::cerr << "incorrect number of arguments provided!\n";
-    std::exit(-1);
+    std::exit(EXIT_FAILURE);
   }
 
   std::ifstream fin;
@@ -26,15 +56,17 @@ int main(int argc, char *argv[]) {
 
   if (!fin) {
     std::cerr << "unable to open file: " << argv[1] << "!\n";
-    std::exit(-1);
+    std::exit(EXIT_FAILURE);
   }
 
   std::string inputval;
-  std::vector<std::string> input;
+  std::set<int> input;
 
   while (std::getline(fin, inputval)) {
-    input.push_back(inputval);
+    input.insert(std::atoi(inputval.c_str()));
   }
 
-  return 0;
+  std::cout << part1(input) << std::endl;
+
+  return EXIT_SUCCESS;
 }
