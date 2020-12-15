805b34ca650980a00ceca7c0a413b94bc0358d12
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2020-12-15 11:42:31 +0000

    Add solution

--- 2020/cxx/day15/src/day15.cpp
+++ 2020/cxx/day15/src/day15.cpp
@@ -8,9 +8,15 @@
 #include <iostream>
 #include <map>
 #include <numeric>
+#include <set>
 #include <sstream>
+#include <string>
 #include <utility>
 #include <vector>
+
+using ull = unsigned long long;
+
+const ull max_turn = 2020;
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -27,12 +33,30 @@ int main(int argc, char *argv[]) {
   }
 
   std::string inputval;
+  // of the pair first -> second to last turn spoken, second -> last turn spoken
+  std::map<ull, std::pair<ull, ull>> spoken;
+  ull turn = 1;
+  ull last = 0;
 
-  while (std::getline(fin, inputval)) {
-    auto input = inputval;
+  while (std::getline(fin, inputval, ',')) {
+    last = std::stoull(inputval);
+    spoken[last] = {0, turn};
+    turn++;
   }
 
-  // std::cout << part1() << std::endl;
+  for (; turn <= max_turn; ++turn) {
+    last = spoken[last].first ? spoken[last].second - spoken[last].first : 0;
+
+    // update last spoken
+    if (spoken.count(last)) {
+      spoken[last].first = spoken[last].second;
+      spoken[last].second = turn;
+    } else {
+      spoken[last] = {0, turn};
+    }
+  }
+
+  std::cout << last << std::endl;
 
   return EXIT_SUCCESS;
 }
