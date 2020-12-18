6ba2633f06868746d2d60cd5ab5f9e2cb5f0857e
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2020-12-18 21:22:51 +0000

    Add day skeleton

---
+++ 2020/cxx/day17/src/day17.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+using ull = unsigned long long;
+using sll = long long;
+
+int main(int argc, char *argv[]) {
+  std::ifstream fin;
+  fin.open(argv[1]);
+
+  std::string inputval;
+
+  while (std::getline(fin, inputval)) {
+    std::string input;
+  }
+
+  // std::cout << part1 << '\n';
+  // std::cout << part2 << '\n';
+
+  return EXIT_SUCCESS;
+}
