32f5fe22393c1cfcfeeb52ea8008e38bdd3166c8
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2022-04-06 10:45:17 +0100

    Add skeleton source files

---
+++ 2021/sca/day01/src/main/scala/day01.scala
@@ -0,0 +1,9 @@
+package day01
+
+object App {
+  def main(args: Array[String]): Unit = {
+    println(greeting())
+  }
+
+  def greeting(): String = "Hello, world!"
+}
---
+++ 2021/sca/day02/src/main/scala/day02.scala
@@ -0,0 +1,9 @@
+package day02
+
+object App {
+  def main(args: Array[String]): Unit = {
+    println(greeting())
+  }
+
+  def greeting(): String = "Hello, world!"
+}
